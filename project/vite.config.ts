cc8f282b6f79d7e48f3335b5b5861d4ad34d5779
VitoS <ikn0@proton.me>
2025-03-07 22:17:30 +0100

    Configure GitHub Pages deployment

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/html-indentation/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
